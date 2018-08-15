fb2f2c2494f85a565dbe5cafcc40d6bf898ca15a
Leon Senft <leonsenft@google.com>
2018-08-15 14:56:40 -0700

    Rewrite `dot` abbreviation as a function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@
 test -z "$TMUX"; or exit
 
 if status --is-interactive
-  abbr --add dot "git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
   abbr --add ta 'tmux attach-session -d -t'
   abbr --add tl 'tmux list-sessions'
   abbr --add tn 'tmux new-session -s'
