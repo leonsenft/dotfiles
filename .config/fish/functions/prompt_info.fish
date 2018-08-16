2c36dd59542e9a022081057fc797fb6078b9ed03
Leon Senft <leonsenft@google.com>
2018-08-16 14:07:27 -0700

    Refactor git information out of fish_prompt into prompt_info

---
+++ .config/fish/functions/prompt_info.fish
@@ -0,0 +1,29 @@
+function prompt_info
+  if __fish_is_git_repository
+    # Color branch name depending on status of staged files.
+    set -l branch_color
+    if _git_has_unstaged_changes
+      set branch_color red
+    else if _git_has_staged_changes
+      set branch_color brown
+    else
+      set branch_color green
+    end
+    set -g __fish_git_prompt_color_branch $branch_color
+    set -g __fish_git_prompt_showupstream git
+    set -g __fish_git_prompt_char_upstream_equal ""
+    set -g __fish_git_prompt_char_upstream_behind "↓"
+    set -g __fish_git_prompt_char_upstream_ahead "↑"
+    set -g __fish_git_prompt_char_upstream_diverged "↓↑"
+    echo (__fish_git_prompt "%s ")
+  end
+end
+
+function _git_has_staged_changes
+  not git diff-index --quiet --cached HEAD -- ^/dev/null
+end
+
+function _git_has_unstaged_changes
+  not git diff-files --quiet ^/dev/null
+end
+
